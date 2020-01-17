7648f790734a3728340e420d9d4f956fcd419833
laura <lauradondiego@gmail.com>
2020-01-17 12:12:13 -0500

    completed ex2 in hashtables

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,23 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    for i in tickets:
+        # dont forget hashtable
+        hash_table_insert(hashtable, i.source, i.destination)
 
+    current_ticket = hash_table_retrieve(hashtable, "NONE")
+    # ^ get the first one
+    while current_ticket is not "NONE":
+        for i in range(len(route)):
+            route[i] = current_ticket  # i think route is like storage here
+
+            current_ticket = hash_table_retrieve(hashtable, current_ticket)
+            # ^ new current ticket
+            if current_ticket is "NONE":
+                route[i+1] = current_ticket  # if its last, go next insert
+                break
+
+# Your function should output an array of strings with
+# the entire route of your trip.
+# start airport NONE, end airport NONE
     return route
